4c0a3c2ad0fa221e3260e48ee02a9d3b9c7f37b7
Nguyen Ngoc Thanh 20153393 <thanh.nn153393@sis.hust.edu.vn>
2019-02-20 09:29:25 +0700

    Add createAt for users

---
+++ database/migrations/20190220021910-add-createAt-column-for-users.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('users', 'createAt', Sequelize.DATE)
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  },
+}
