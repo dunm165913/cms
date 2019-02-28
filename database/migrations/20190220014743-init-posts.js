1e3c9d259d9ba5dde6f319544c05f9bb5611bfe0
Nguyen Ngoc Thanh 20153393 <thanh.nn153393@sis.hust.edu.vn>
2019-02-28 14:09:11 +0700

    Update migrate

--- database/migrations/20190220014743-init-posts.js
+++ database/migrations/20190220014743-init-posts.js
@@ -2,11 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const { INTEGER, STRING } = Sequelize
+    const { INTEGER, STRING, DATE } = Sequelize
     await queryInterface.createTable('posts', {
       id: { type: INTEGER, autoIncrement: true, primaryKey: true },
       title: { type: STRING(255), allowNull: false },
       content: { type: STRING, allowNull: false },
+      createAt: { type: DATE, allowNull: false },
       user_id: {
         type: INTEGER,
         references: {
